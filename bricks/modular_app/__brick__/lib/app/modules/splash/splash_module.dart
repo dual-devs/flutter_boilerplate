2b92c0e5ce8cc6bb63b5afb2bc3bfd582e3da9d2
EdDualDevs <edvanio@dualdevs.dev>
2023-05-27 18:48:02 -0300

    correction import

--- bricks/modular_app/__brick__/lib/app/modules/splash/splash_module.dart
+++ bricks/modular_app/__brick__/lib/app/modules/splash/splash_module.dart
@@ -1,6 +1,7 @@
-import 'package:flutter_modular_base_app/app/modules/splash/splash_page.dart';
-import 'package:flutter_modular_base_app/app/modules/splash/splash_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import 'splash_page.dart';
+import 'splash_store.dart';
 
 class SplashModule extends Module {
   @override
